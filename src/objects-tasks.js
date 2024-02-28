92474aac99bb5960455219b77a488eac2ae25dd3
Mikita <nikitakuza1999@gmail.com>
2024-02-28 01:16:05 +0100

    feat: add solution for tasks: 'removeProperties', 'compareObjects' and 'isEmptyObject'

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -49,8 +49,12 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const copyObj = { ...obj };
+  for (let i = 0; i < keys.length; i += 1) {
+    delete copyObj[keys[i]];
+  }
+  return copyObj;
 }
 
 /**
@@ -65,8 +69,8 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
@@ -80,8 +84,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
